9d437b527042065fd3461d80be88747e1b734d82
kkkstp <chassse18@gmail.com>
2023-11-23 17:56:33 +0500

    ...

--- someproject/firstapp/urls.py
+++ someproject/firstapp/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from firstapp import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('index/', views.index, name='index'),
+    path('add_person/', views.add_person, name='add_person'),
+    path('contacts/', views.show_contacts, name='show_contacts'),
+    path('about/', views.show_about, name='show_about'),
 ]
--- someproject/firstapp/views.py
+++ someproject/firstapp/views.py
@@ -1,14 +1,43 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 
-menu = [{'title': "Main page", 'url_name': 'mainpage'},
-        {'title': "Add person", 'url_name': 'add_person'},
-        {'title': "Contacts", 'url_name': 'contact'},
-        {'title': "About", 'url_name': 'about'}
+menu = [{'title': 'Main page', 'url_name': 'index'},
+        {'title': 'Add person', 'url_name': 'add_person'},
+        {'title': 'Contacts', 'url_name': 'show_contacts'},
+        {'title': 'About', 'url_name': 'show_about'}
 ]
 def index(request):
+    title, url_name = menu[0]['title'], menu[0]['url_name']
     data = {
-        'title': 'Main page',
+        'title': title,
         'menu': menu,
+        'url_name': url_name,
     }
-    return render(request, 'firstapp/index.html')
+    return render(request, 'firstapp/index.html', context=data)
+
+def add_person(request):
+    title, url_name = menu[1]['title'], menu[1]['url_name']
+    data = {
+        'title': title,
+        'menu': menu,
+        'url_name': url_name,
+    }
+    return render(request, 'firstapp/add_person.html', context=data)
+
+def show_contacts(request):
+    title, url_name = menu[2]['title'], menu[2]['url_name']
+    data = {
+        'title': title,
+        'menu': menu,
+        'url_name': url_name,
+    }
+    return render(request, 'firstapp/show_contacts.html', context=data)
+
+def show_about(request):
+    title, url_name = menu[3]['title'], menu[3]['url_name']
+    data = {
+        'title': title,
+        'menu': menu,
+        'url_name': url_name,
+    }
+    return render(request, 'firstapp/show_about.html', context=data)
