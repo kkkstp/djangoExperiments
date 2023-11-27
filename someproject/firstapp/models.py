1ae46367091c263599b8e4e666bba816739bebe5
kkkstp <chassse18@gmail.com>
2023-11-27 20:50:58 +0500

    need

--- someproject/firstapp/models.py
+++ someproject/firstapp/models.py
@@ -4,3 +4,10 @@ from django.urls import reverse
 class Men(models.Model):
     name = models.CharField(max_length=100)
     age = models.IntegerField()
+    second_name = models.CharField(max_length=100, blank=True)
+    slug = models.SlugField(db_index=True, unique=True, blank=True)
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('men', kwargs={'slug': self.slug})
--- someproject/firstapp/views.py
+++ someproject/firstapp/views.py
@@ -1,11 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
+from .models import Men
 
 menu = [{'title': 'Main page', 'url_name': 'index'},
         {'title': 'Table', 'url_name': 'table'},
         {'title': 'Contacts', 'url_name': 'show_contacts'},
         {'title': 'About', 'url_name': 'show_about'}
 ]
+
+links = ['one', 'two', 'three', 'four', 'five']
+Men = Men.objects.all()
 def index(request):
     title, url_name = menu[0]['title'], menu[0]['url_name']
     data = {
@@ -21,6 +25,8 @@ def table(request):
         'title': title,
         'menu': menu,
         'url_name': url_name,
+        'links': links,
+        'Men': Men,
     }
     return render(request, 'firstapp/table.html', context=data)
 
@@ -41,3 +47,12 @@ def show_about(request):
         'url_name': url_name,
     }
     return render(request, 'firstapp/show_about.html', context=data)
+
+def show_person(request):
+    title, url_name = menu[1]['title'], menu[1]['url_name']
+    data = {
+        'title': title,
+        'menu': menu,
+        'url_name': url_name,
+    }
+    return render(request, 'firstapp/show_person.html', context=data)
