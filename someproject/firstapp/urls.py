404ed606c65a74810e7a7a1ba7f7af3b4c45e4f3
kkkstp <chassse18@gmail.com>
2023-11-30 18:34:47 +0500

    ...

--- someproject/firstapp/urls.py
+++ someproject/firstapp/urls.py
@@ -2,10 +2,5 @@ from django.urls import path
 from firstapp import views
 
 urlpatterns = [
-    path('index/', views.index, name='index'),
-    path('table/', views.table, name='table'),
-    path('professions/<slug:name_slug>', views.show_professions, name='show_professions'),
-    path('contacts/', views.show_contacts, name='show_contacts'),
-    path('about/', views.show_about, name='show_about'),
-    path('men/<slug:k_slug>/', views.show_person, name='show_person'),
+    path('', views.index, name='index'),
 ]
--- someproject/firstapp/views.py
+++ someproject/firstapp/views.py
@@ -8,66 +8,11 @@ menu = [{'title': 'Main page', 'url_name': 'index'},
         {'title': 'About', 'url_name': 'show_about'}
 ]
 
-linksOfProfession = Professions.objects.all()
-Men_0 = Men.objects.all()
-
 def index(request):
-    title, url_name = menu[0]['title'], menu[0]['url_name']
+    title, url_name = 'Main page', 'index'
     data = {
         'title': title,
         'menu': menu,
         'url_name': url_name,
     }
     return render(request, 'firstapp/index.html', context=data)
-
-def table(request):
-    title, url_name = menu[1]['title'], menu[1]['url_name']
-    data = {
-        'title': title,
-        'menu': menu,
-        'url_name': url_name,
-        'linksOfProfession': linksOfProfession,
-        'Men': Men_0,
-    }
-    return render(request, 'firstapp/table.html', context=data)
-
-def show_professions(request, name_slug):
-    title, url_name = "а че", menu[2]['url_name']
-    abs = Professions.objects.get(slug=name_slug)
-    linksOfProfession = abs.men_set.all()
-    data = {
-        'title': title,
-        'menu': menu,
-        'url_name': url_name,
-        'links': linksOfProfession,
-    }
-
-    return render(request, 'firstapp/professions.html', context=data)
-
-def show_contacts(request):
-    title, url_name = menu[2]['title'], menu[2]['url_name']
-    data = {
-        'title': title,
-        'menu': menu,
-        'url_name': url_name,
-    }
-    return render(request, 'firstapp/show_about.html', context=data)
-
-def show_about(request):
-    title, url_name = menu[3]['title'], menu[3]['url_name']
-    data = {
-        'title': title,
-        'menu': menu,
-        'url_name': url_name,
-    }
-    return render(request, 'firstapp/show_about.html', context=data)
-
-def show_person(request, k_slug):
-    men = get_object_or_404(Men, slug=k_slug)
-    title, url_name = menu[1]['title'], menu[1]['url_name']
-    data = {
-        'title': title,
-        'menu': menu,
-        'url_name': url_name,
-    }
-    return render(request, 'firstapp/show_person.html', context=data)
